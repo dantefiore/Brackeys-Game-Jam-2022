0e60bb58e63fb824d83affcecc08b0469e8d426f
mikebaran92 <mikebaran92@gmail.com>
2022-08-26 20:49:30 -0400

    Mike Push 5
    Sign Layouts + Sign code

--- Assets/Scripts/World/Sign.cs
+++ Assets/Scripts/World/Sign.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Sign : MonoBehaviour
 {
+    public GameObject nextSign;
+
+
     //[SerializeField] private GameObject thisSign;
-    [SerializeField] private GameObject nextSign;
+    /*[SerializeField] private GameObject nextSign;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,5 +21,13 @@ public class Sign : MonoBehaviour
         yield return new WaitForSeconds(2f);
         this.gameObject.SetActive(false);
         nextSign.SetActive(true);
+    }*/
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            nextSign.SetActive(true);
+        }
     }
 }
